25cdb6cfafbf0c0513b8a2c1607de7288892c582
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-27 10:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btn.addEventListener('click', async () => {
+	try{
+	   const text = document.getElementById("text").value;
+	   const delay = document.getElementById("delay").value;
+		const message = await displayWithDelay(text, delay);
+		output.textContent = message;
+	}catch(error){
+		console.log("Error: ", error);
+		output.textContent = "Invalid delay or other error Ocured!";
+	}
+});
+
+async function displayWithDelay(message, delay){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(message);
+		},delay);
+	});
+}
